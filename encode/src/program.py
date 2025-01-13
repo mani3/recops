dc60e1aad6c535cbb03c1f78136f5f74d330e16d
mani3 <kazuya.4da@gmail.com>
2025-01-13 22:31:50 +0900

    :hammer: add lazy decoder

--- encode/src/program.py
+++ encode/src/program.py
@@ -3,6 +3,17 @@ import json
 import re
 
 
+class LazyDecoder(json.JSONDecoder):
+  def decode(self, s, **kwargs):
+    regex_replacements = [
+      (re.compile(r"([^\\])\\([^\\])"), r"\1\\\\\2"),
+      (re.compile(r",(\s*])"), r"\1"),
+    ]
+    for regex, replacement in regex_replacements:
+      s = regex.sub(replacement, s)
+    return super().decode(s, **kwargs)
+
+
 class Program:
   def __init__(self, json_file, raw_filename):
     self.json_file = json_file
@@ -28,7 +39,7 @@ class Program:
       raise ValueError(f"Invalid filename: {self.raw_filename}")
 
     with open(self.json_file, "r") as file:
-      data = json.load(file)
+      data = json.load(file, cls=LazyDecoder)
 
     for program in data:
       programs = program["programs"]
